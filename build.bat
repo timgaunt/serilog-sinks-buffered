af7e657488f1a05464f0cbaa80a4e235ea5f4b64
Tim Gaunt <tim@thesitedoctor.co.uk>
2015-04-05 16:14:34 +0100

    Added initial build and nuspec files

---
+++ build.bat
@@ -0,0 +1,24 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+ 
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+ 
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\SerilogSinksBuffered.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+ 
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net40
+ 
+%nuget% pack "src\SerilogSinksBuffered.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "src\SerilogSinksBuffered.Web.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
